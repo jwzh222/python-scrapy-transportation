6c36c25f0ffa467716380de097ecbb2346e50aaf
jwzh222 <jwzheng0410@hotmail.com>
2016-06-07 11:51:46 +0800

    Update scraping_threads.py
    增加注释，编码参照pep8规范修改

--- scraping_threads.py
+++ scraping_threads.py
@@ -1,18 +1,26 @@
-
 #-*- coding:utf-8 -*-
+
+'''多线程爬取数据插入到Mysql中final_data表
+ThreadUrl类读入Queue中的数据，形成表单post请求到网站，将抓取的数据写入out_queue
+DatamineThread类读入out_queue中的数据，数据清理，写入数据库
+
+'''
 import requests
 import cookielib
 import json
-from bs4 import BeautifulSoup
-from time import sleep
 import random
 import csv
 import datetime
+import Queue
+import threading
+import pymysql
 
+from bs4 import BeautifulSoup
+from time import sleep
+
+# login and save cookies
 url='https://www.hoau.net/how/bse/showPriceTime.action'
 url_query='https://www.hoau.net/how/bse/queryPriceTime.action'
-
-
 requests = requests.Session()
 requests.cookies = cookielib.LWPCookieJar('cookies')
 try:
@@ -31,7 +39,7 @@ headers = {
 requests.get(url,headers=headers)
 requests.cookies.save()
 
-
+# POST form_data to site and get the data we want
 def get_price(conCity,conCounty,shipperCity,shipperCounty):
 	form_data={'priceQueryVo.conCity':conCity,'priceQueryVo.conCounty':conCounty,
 'priceQueryVo.shipperCity':shipperCity,'priceQueryVo.shipperCounty':shipperCounty}
@@ -56,10 +64,6 @@ def get_price(conCity,conCounty,shipperCity,shipperCounty):
 	return (conCity,conCounty,shipperCity,shipperCounty,deliveryTime,startPrice,heavyPrice,lightPrice)
 
 
-############
-import Queue
-import threading
-import pymysql
 
 queue = Queue.Queue()
 out_queue = Queue.Queue()
@@ -75,9 +79,7 @@ def gen_queue(num1,num2):
 	#conn.close()
 	return cc_list
 
-
-
-
+	
 # get queue, deal, put out_queue
 class ThreadUrl(threading.Thread):
 	def __init__(self, queue, out_queue):
@@ -93,9 +95,6 @@ class ThreadUrl(threading.Thread):
 				_id=threading.current_thread().getName()
 				print "the thread:",_id,"scraping another 50 times!",i
 				sleep(random.uniform(1,1.5))
-
-			
-			
 			try:
 				cc = self.queue.get()
 				data=get_price(*cc)  #data=(u'\u5317\u4eac\u5e02', u'\u4e1c\u57ce\u533a', u'\u5317\u4eac\u5e02', u'\u4e1c\u57ce\u533a', 15.0, 0.6, 132.0)
@@ -104,16 +103,9 @@ class ThreadUrl(threading.Thread):
 				#print "ThreadUrl get_price() error!"
 				#print "%s"%(str(e))
 				pass
+			self.queue.task_done()
 
 			
-
-			self.queue.task_done()
-			#_id=threading.current_thread().getName()
-			#print _id,i
-
-
-
-
 # get out_queue, deal
 class DatamineThread(threading.Thread):
 	def __init__(self, out_queue):
@@ -136,12 +128,10 @@ class DatamineThread(threading.Thread):
 			try:
 				cur.execute(sqli,(chunk[0].encode('utf-8'),chunk[1].encode('utf-8'),chunk[2].encode('utf-8'),chunk[3].encode('utf-8'),chunk[4].encode('utf-8'),chunk[5],chunk[6],chunk[7]))
 				cur.execute("commit")
-
 			except Exception, e:
 				print "DatamineThread insert error!"
 				#print "%s"%(str(e))
 				#print chunk
-
 			#check if lost connection to Mysql
 			if(i%50==0):
 				_id=threading.current_thread().getName()
@@ -149,65 +139,53 @@ class DatamineThread(threading.Thread):
 				try:
 					cur.execute("select 1 from final_data")
 					a=cur.fetchone()
-					#print "cur.fetchone()=",a
 					check=a[0]
 					if(check==1):
 						print _id,'db insert threads connected ok!'
 				except:
 					print _id,"seems lost connection to MySQL!"
-					#cur.execute("select count(1) from final_data")
-					#print _id,cur.fetchone()
 					conn = pymysql.connect(host='127.0.0.1',user='root', passwd='123456', db='mysql', charset='utf8')
 					conn.ping(True)
 					cur = conn.cursor()
 					cur.execute("USE scrapy")
 				
 			self.out_queue.task_done()
-			#_id=threading.current_thread().getName()
-			#print _id,i
-
 
 
 def goscrapy():
-	
-	
+
 	for i in range(10):
 		t=ThreadUrl(queue,out_queue)
 		t.setDaemon(True)
 		t.start()
-
 	for i in range(10):
 		dt = DatamineThread(out_queue)
 		dt.setDaemon(True)
 		dt.start()
-
-
 	cc_list=gen_queue(37450,1000000)#12054784=6054784+6000000   #1w 
 	for cc in cc_list:
 		queue.put(cc)
 	print "queue is ready!"
-
 	#wait until processing finish	
 	queue.join()
 	out_queue.join()
 
-
-
 ## main
-begin=datetime.datetime.now()
-conn = pymysql.connect(host='127.0.0.1',user='root', passwd='123456', db='mysql', charset='utf8')
-#conn.ping(True)
-cur = conn.cursor()
-cur.execute("USE scrapy")
+def main():
 
-goscrapy()
+	begin=datetime.datetime.now()
+	conn = pymysql.connect(host='127.0.0.1',user='root', passwd='123456', db='mysql', charset='utf8')
+	#conn.ping(True)
+	cur = conn.cursor()
+	cur.execute("USE scrapy")
+	goscrapy()
+	end=datetime.datetime.now()
+	print 'Total times used:',(end-begin)
+	cur.close()
+	conn.close()
 
-end=datetime.datetime.now()
-print 'Total times used:',(end-begin)
-cur.close()
-conn.close()
-
-
+if __name__ == "__main__":
+	main()
 
 
 
